633ca4453904a4863ea9fc94323debfec1e42c28
Artem Andriushchenko <link.by.artem@gmail.com>
2023-08-18 05:42:57 +0300

    add Question 27-30

---
+++ src/nix_beginner_test/Question_27.java
@@ -0,0 +1,16 @@
+package nix_beginner_test;
+
+public class Question_27 {
+
+    /*
+     Які URL у правильному форматі?
+     Виберіть одну або декілька відповідей:
+
+     1) data:text/plain;charset=iso-8859-7,%be%be%be ✔
+     2) https://ru.wikipedia.org/wiki/%D0%A3%D0%A0%D0%9B ✔
+     3) https://prnt.sc/vp8zdz ✔
+     4) tel:+1-816-555-1212 ✔
+     5) news:comp.infosystems.www.servers.unix ✔
+
+    */
+}
---
+++ src/nix_beginner_test/Question_29.java
@@ -0,0 +1,25 @@
+package nix_beginner_test;
+
+public class Question_29 {
+
+    /*
+      Как параметры передаются в POST запросе?
+      Выберите один ответ:
+
+      1) в header
+      2) в url
+      3) в типе
+      4) в схеме
+      5) в теле ✔
+
+      Параметры в POST запросе передаются в теле HTTP запроса, а не в URL, как это происходит в GET запросе.
+      По сравнению с GET запросами, POST запросы чаще используются для отправки более крупных и сложных данных, таких как формы,
+      загрузки файлов и другие типы контента.
+
+      POST /path/to/resource HTTP/1.1
+      Host: example.com
+      Content-Type: application/x-www-form-urlencoded
+      param1=value1&param2=value2&param3=value3
+
+    */
+}
